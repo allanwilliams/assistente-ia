cab1bb4c1e22a9994c1b20033b579d83746259dd
Tyago Nunes <tyago.nunes@defensoria.ce.def.br>
2024-02-16 16:44:01 -0300

    integrando com chat PDF

--- apps/documento/api/viewsets.py
+++ apps/documento/api/viewsets.py
@@ -1,16 +1,61 @@
 from rest_framework.viewsets import ModelViewSet
 from apps.documento.models import Chat, Mensagem
 from apps.documento.api.serializers import ChatSerializer, MensagemSerializer
-
+import requests
+from rest_framework.response import Response
+from rest_framework import status
+import json
+from apps.documento.choices import CHAT_AUTOR_IA
 
 
 class ChatViewSet(ModelViewSet):
     queryset = Chat.objects.all()
     serializer_class = ChatSerializer
     http_method_names = ['get', 'patch', 'post', 'delete','put']
-    
 
 class MensagemViewSet(ModelViewSet):
     queryset = Mensagem.objects.all()
     serializer_class = MensagemSerializer
     http_method_names = ['get', 'patch', 'post', 'delete','put']
+
+
+    def create(self, request, *args, **kwargs):
+
+        chat = request.POST.get('chat')
+        texto = request.POST.get('texto')
+        autor = request.POST.get('autor')
+
+        nova_msg = Mensagem(chat_id=chat, texto=texto, autor=autor)
+        nova_msg.save()
+
+        chatpdf_source_id = nova_msg.chat.chatpdf_source_id
+
+        headers = {
+            'x-api-key': 'sec_CPSnpaMaVcZyatxIozz9Jv8saovN1tbN',
+            "Content-Type": "application/json",
+        }
+
+        data = {
+            "referenceSources": True,
+            'sourceId': chatpdf_source_id,
+            'messages': [
+                {
+                    'role': "user",
+                    'content': texto,
+                }
+            ]
+        }
+
+        response = requests.post(
+            'https://api.chatpdf.com/v1/chats/message', headers=headers, json=data)
+
+        resposta_chatpdf = Mensagem(chat_id=chat, texto='', autor=CHAT_AUTOR_IA)
+        
+        if response.status_code == 200:
+            resposta_chatpdf.texto = response.json()['content']
+        else:
+            resposta_chatpdf.texto = 'Erro ao responder'
+
+        resposta_chatpdf.save()
+
+        return Response({ "texto": resposta_chatpdf.texto, "autor": resposta_chatpdf.autor }, status=status.HTTP_201_CREATED)
--- apps/documento/models.py
+++ apps/documento/models.py
@@ -4,12 +4,14 @@ from apps.documento.choices import CHOICES_CHAT_AUTOR, CHAT_AUTOR_IA
 from datetime import datetime
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-
+import requests
+import os
 
 class Chat(BaseModel):
     titulo = models.CharField('Titulo', max_length=255)
     documento = models.FileField('Documento', upload_to='documento_chat')
     ativo = models.BooleanField('Ativo', default=True)
+    chatpdf_source_id = models.CharField('Chat PDF source id', max_length=255, blank=True, null=True)
 
     def __str__(self) -> str:
         return f'{self.id}'
@@ -17,15 +19,37 @@ class Chat(BaseModel):
 
 @receiver(post_save, sender=Chat)
 def criar_mensagens_chat(sender, instance, **kwargs):
-    if instance:
-        tem_mensagens = Mensagem.objects.filter(chat_id=instance.id).exists()
 
-        if not tem_mensagens:
-            try:
-                nova_mensagem = Mensagem(texto="Bem vindo ao dede chat", chat_id=instance.id, autor=CHAT_AUTOR_IA, criado_em=datetime.now())
+    if instance and not instance.chatpdf_source_id:
+        ROOT = os.path.abspath(os.path.dirname(f'media/documento_chat'))
+        file_path = '{}/{}'.format(ROOT, instance.documento)
+    
+        try:
+            with open(file_path, 'rb') as file:
+            
+                files = [
+                    ('file', ('file', file, 'application/octet-stream'))
+                ]
+                headers = {
+                    'x-api-key': 'sec_CPSnpaMaVcZyatxIozz9Jv8saovN1tbN'
+                }
+
+                response = requests.post(
+                    'https://api.chatpdf.com/v1/sources/add-file', headers=headers, files=files)
+
+                msg_inicial = 'Bem vindo ao dede chat. Faça a primeira pergunda.'
+
+                if response.status_code == 200:
+                    instance.chatpdf_source_id = response.json()['sourceId']
+                    instance.save()
+                else:
+                    msg_inicial = 'Houve um erro ao processar o PDF'
+
+
+                nova_mensagem = Mensagem(texto=msg_inicial, chat_id=instance.id, autor=CHAT_AUTOR_IA, criado_em=datetime.now())
                 nova_mensagem.save()
-            except Exception as e:
-                print('erro', e)
+        except Exception as e:
+            print('erro', e)
 
 
 class Mensagem(BaseModel):
