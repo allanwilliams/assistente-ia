580dc90f4487a9f4b396d521995f7ea331f5f16e
Tyago Nunes <tyago.nunes@defensoria.ce.def.br>
2024-03-18 11:27:47 -0300

    modal de editor de avatar com cores

--- apps/documento/models.py
+++ apps/documento/models.py
@@ -117,7 +117,7 @@ class Transcricao(BaseModel):
     tempo_inicial_segundos = models.IntegerField("Tempo inicial em segundos", default=0)
     tempo_final = models.CharField("Tempo final",max_length=12)
     speaker = models.CharField("Speaker",max_length=50, blank=True, null=True)
-    cor_speaker = models.CharField('Cor do Speaker', max_length=50, default="#337ab7")
+    cor_speaker = models.CharField('Cor do Speaker', max_length=50, default="#0000FF")
 
     class Meta:
         ordering = ['id',]
