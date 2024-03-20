e4a03ce9edc27a25249d8a1635e13fe57b94b778
Tyago Nunes <tyago.nunes@defensoria.ce.def.br>
2024-03-20 14:31:43 -0300

    adicionando funcao de baixar transc por txt

--- apps/documento/urls.py
+++ apps/documento/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path, include
-from .views import chat,dashboard, transcricao_video, transcricao, export_chat_txt
+from .views import chat,dashboard, transcricao, export_chat_txt, export_transcricoes_txt
 
 app_name = "documento"
 urlpatterns = [
     path("api/", include("apps.documento.api.urls")),
     path('chat/', chat, name='chat'),
     path('export-chat-txt/<id>', export_chat_txt, name='export_chat_txt'),
+    path('export-transcricoes-txt/<id>', export_transcricoes_txt, name='export_transcricoes_txt'),
     path('transcricao/', transcricao, name='transcricao'),
     # path('dashboard/', dashboard, name='dashboard'),
     path('dashboard-media/', dashboard, name='dashboard_media'),
     path('dashboard-documento/', dashboard, name='dashboard_documento'),
-    path('transcricao-video/', transcricao_video, name='transcricao_video'),
 ]
