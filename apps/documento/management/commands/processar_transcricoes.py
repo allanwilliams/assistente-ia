45234dc4a7db905f7721e7e76d3ce3bbfa171f43
Tyago Nunes <tyago.nunes@defensoria.ce.def.br>
2024-03-22 14:28:41 -0300

    command para processar transcricoes

---
+++ apps/documento/management/commands/processar_transcricoes.py
@@ -0,0 +1,20 @@
+from django.core.management.base import BaseCommand, CommandError
+from apps.documento.choices import STATUS_PROCESSANDO_ARQUIVO, STATUS_FAZENDO_TRANSCRICAO, STATUS_FILA_PROCESSAMENTO
+from apps.documento.models import MediaTranscricao
+from apps.documento.utils import preparar_audio
+
+def processar_transcricoes():
+    ocupado = MediaTranscricao.objects.filter(status__in=[STATUS_FAZENDO_TRANSCRICAO, STATUS_PROCESSANDO_ARQUIVO], ativo=True).exists()
+    print('Executando ......')
+    if not ocupado:
+        transcricao = MediaTranscricao.objects.filter(status=STATUS_FILA_PROCESSAMENTO).order_by('id').first()
+        if transcricao:
+            print('Processando...')
+            preparar_audio(transcricao.id)
+
+
+class Command(BaseCommand):
+    help = 'Processar Media'
+
+    def handle(self, *args, **options):
+        processar_transcricoes()
