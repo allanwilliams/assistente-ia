06cf4428d87a58405a8d4bc37749d1094c67573b
Tyago Nunes <tyago.nunes@defensoria.ce.def.br>
2024-04-11 16:24:06 -0300

    sh

--- processar_transcricoes.sh
+++ processar_transcricoes.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-source /home/defensoria/assistente_ia/venv/bin/activate 
+source /home/defensoria/assistente-ia/venv/bin/activate 
 
-cd /home/defensoria/assistente_ia 
+cd /home/defensoria/assistente-ia 
 
 python manage.py processar_transcricoes
