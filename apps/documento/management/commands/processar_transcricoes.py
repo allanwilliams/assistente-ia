db29f87dc5ba2154694390988f1fdbcc84ac9188
Tyago Nunes <tyago.nunes@defensoria.ce.def.br>
2024-06-20 14:09:32 -0300

    melhorando fluxo de processamento

--- apps/documento/management/commands/processar_transcricoes.py
+++ apps/documento/management/commands/processar_transcricoes.py
@@ -1,10 +1,10 @@
 from django.core.management.base import BaseCommand, CommandError
-from apps.documento.choices import STATUS_PROCESSANDO_ARQUIVO, STATUS_FAZENDO_TRANSCRICAO, STATUS_FILA_PROCESSAMENTO
+from apps.documento.choices import STATUS_PROCESSANDO_ARQUIVO, STATUS_FILA_PROCESSAMENTO
 from apps.documento.models import MediaTranscricao
 from apps.documento.utils import preparar_audio
 
 def processar_transcricoes():
-    ocupado = MediaTranscricao.objects.filter(status__in=[STATUS_FAZENDO_TRANSCRICAO, STATUS_PROCESSANDO_ARQUIVO], ativo=True).exists()
+    ocupado = MediaTranscricao.objects.filter(status=STATUS_PROCESSANDO_ARQUIVO, ativo=True).exists()
     print('Executando ......')
     if not ocupado:
         transcricao = MediaTranscricao.objects.filter(status=STATUS_FILA_PROCESSAMENTO, ativo=True).order_by('id').first()
