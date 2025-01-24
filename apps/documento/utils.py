cd7cef146bf30ed2f077f78d919f01028a60c7ae
allanwilliams <47225390+allanwilliams@users.noreply.github.com>
2025-01-24 15:28:52 -0300

    vai

--- apps/documento/utils.py
+++ apps/documento/utils.py
@@ -244,8 +244,7 @@ class TanakaUtils:
             max_new_tokens=max_new_tokens,
             no_speech_threshold=0.6,
             condition_on_previous_text=condition_on_previous_text,
-            initial_prompt=initial_prompt,
-            batch
+            initial_prompt=initial_prompt
         )
 
         transcription_segments = transcription_with_progress_bar(
